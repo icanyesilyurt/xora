7afd9c7c7dd75b8f78cfcb63b828fc958f2b6dfc
icanyesilyurt <ican.yslyrt@gmail.com>
2026-06-12 14:49:40 +0300

    Update Supabase publishable key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.XORA_CONFIG = {
   SUPABASE_URL: "https://jafrxhkppvvchyjbkldd.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_KYv-Rtqh0X-59zmshoJiyw_zulHWTOI",
+  SUPABASE_ANON_KEY: "sb_publishable_57Kowrhv4k30FoEDMBsIaw_WDh1c8kC",
   AUTH_REDIRECT_URL: "https://icanyesilyurt.github.io/xora/mirror.html"
 };
