592bfb4a84fdd74f701812431e3a2ad7e8671f6c
icanyesilyurt <ican.yslyrt@gmail.com>
2026-06-12 14:04:18 +0300

    Clarify Supabase anon key placeholder

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 window.XORA_CONFIG = {
   SUPABASE_URL: "https://jafrxhkppvvchyjbkldd.supabase.co",
-  SUPABASE_ANON_KEY: "PASTE_SUPABASE_ANON_OR_PUBLISHABLE_KEY_HERE",
-
+  SUPABASE_ANON_KEY: "BURAYA_SUPABASE_ANON_PUBLIC_KEY_GELECEK",
   AUTH_REDIRECT_URL: "https://icanyesilyurt.github.io/xora/mirror.html"
 };
